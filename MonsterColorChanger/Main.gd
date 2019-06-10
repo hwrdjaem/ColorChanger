1d0be915b16cd4050f8a96e8b0dfab190822b83d
Jabocus <janatra94@hotmail.com>
2019-06-10 09:39:45 -0400

    Add files via upload

--- MonsterColorChanger/Main.gd
+++ MonsterColorChanger/Main.gd
@@ -3,23 +3,111 @@ extends Node2D
 func _ready():
 	pass
 
-func _on_Button1_pressed(): #green
-	$"Monster/Light".modulate = Color(1, 0.65, 0, 1)
-	$"Monster/Dark".modulate = Color(0, 1, 0, 1)
+func _on_Button1_pressed(): #cyan
+	$"Monster/Dark".modulate = Color(.37, .8, .89, 1)
 
-func _on_Button2_pressed(): #purple
-	$"Monster/Light".modulate = Color(1, 0.71, 0.76, 1)
-	$"Monster/Dark".modulate = Color(0.68, 0.85, 0.9, 1)
+func _on_Button2_pressed(): #wall-olive
+	$"Monster/Dark".modulate = Color(.27, .29, .24, 1)
 
-func _on_Button3_pressed(): #yellow
-	$"Monster/Light".modulate = Color(1, 0.84, 0, 1)
-	$"Monster/Dark".modulate = Color(0, 0.39, 0, 1)
+func _on_Button3_pressed(): #blue-gray
+	$"Monster/Dark".modulate = Color(.33, .35, .44, 1)
 
-func _on_Button4_pressed(): #brown
-	$"Monster/Light".modulate = Color(0.65, 0.16, 0.16, 1)
-	$"Monster/Dark".modulate = Color( 0.1, 0.1, 0.44, 1)
-	
-#How we can integrate this into our current system:
-#For now, roll for colors using a dictionary
-#Use separate dark and light dictionaries
-#each numerical value will have a list that refers to an appropriate RGB sequence
+func _on_Button4_pressed(): #indigo
+	$"Monster/Dark".modulate = Color(.15, .15, .62, 1)
+
+func _on_Button5_pressed(): #wheat
+	$"Monster/Dark".modulate = Color(.71, .52, .32, 1)
+
+func _on_Button6_pressed(): #hunter green
+	$"Monster/Dark".modulate = Color(.05, .25, .17, 1)
+
+func _on_Button7_pressed(): #adobe
+	$"Monster/Dark".modulate = Color(.67, .23, .12, 1)
+
+func _on_Button8_pressed(): #purple
+	$"Monster/Dark".modulate = Color(.14, .11, .33, 1)
+
+func _on_Button9_pressed(): #dark green
+	$"Monster/Dark".modulate = Color(.09, .16, .11, 1)
+
+func _on_Button10_pressed(): #gray
+	$"Monster/Dark".modulate = Color(.25, .25, .25, 1)
+
+func _on_Button11_pressed(): #mustard
+	$"Monster/Dark".modulate = Color(.353, .33, .05, 1)
+
+func _on_Button12_pressed(): #dark teal
+	$"Monster/Dark".modulate = Color(.10, .23, .24, 1)
+
+func _on_Button13_pressed(): #olive
+	$"Monster/Dark".modulate = Color(.21, .25, .15, 1)
+
+func _on_Button14_pressed(): #cerulean
+	$"Monster/Dark".modulate = Color(.11, .20, .31, 1)
+
+func _on_Button15_pressed(): #mud
+	$"Monster/Dark".modulate = Color(.25, .12, .04, 1)
+
+func _on_Button16_pressed(): #maroon
+	$"Monster/Dark".modulate = Color(.20, .04, .15, 1)
+
+func _on_Button17_pressed(): #pale purple
+	$"Monster/Dark".modulate = Color(.20, .11, .22, 1)
+
+func _on_Button18_pressed(): #sky blue
+	$"Monster/Dark".modulate = Color(.39, .61, 1, 1)
+
+func _on_Button19_pressed(): #white
+	$"Monster/Light".modulate = Color(1, 1, 1, 1)
+
+func _on_Button20_pressed(): #gold
+	$"Monster/Light".modulate = Color(.81, .58, .09, 1)
+
+func _on_Button21_pressed(): #lilac
+	$"Monster/Light".modulate = Color(.7, .55, .81, 1)
+
+func _on_Button22_pressed(): #red
+	$"Monster/Light".modulate = Color(.62, .15, .20, 1)
+
+func _on_Button23_pressed(): #red-gray
+	$"Monster/Light".modulate = Color(.27, .27, .34, 1)
+
+func _on_Button24_pressed(): #copper
+	$"Monster/Light".modulate = Color(.74, .39, .15, 1)
+
+func _on_Button25_pressed(): #salmon
+	$"Monster/Light".modulate = Color(1, .71, .71, 1)
+
+func _on_Button26_pressed(): #violet
+	$"Monster/Light".modulate = Color(.43, .9, .33)
+
+func _on_Button27_pressed(): #grass
+	$"Monster/Light".modulate = Color(.58, .65, .39, 1)
+
+func _on_Button28_pressed(): #gray
+	$"Monster/Light".modulate = Color(.55, .55, .55, 1)
+
+func _on_Button29_pressed(): #yellow
+	$"Monster/Light".modulate = Color(.84, .76, .14)
+
+func _on_Button30_pressed(): #periwinkle
+	$"Monster/Light".modulate = Color(100, 136, 235)
+
+func _on_Button31_pressed(): #puma
+	$"Monster/Light".modulate = Color(.71, .64, .47, 1)
+
+func _on_Button32_pressed(): #seafoam
+	$"Monster/Light".modulate = Color(.46, .75, .67, 1)
+
+func _on_Button33_pressed(): #mint
+	$"Monster/Light".modulate = Color(.56, .92, .65, 1)
+
+func _on_Button34_pressed(): #beige
+	$"Monster/Light".modulate = Color(.82, .65, .47, 1)
+
+func _on_Button35_pressed(): #pale yellow
+	$"Monster/Light".modulate = Color(.9, .89, .52, 1)
+
+func _on_Button36_pressed(): #cloud
+	$"Monster/Light".modulate = Color(.8, .86, .99, 1)
+
